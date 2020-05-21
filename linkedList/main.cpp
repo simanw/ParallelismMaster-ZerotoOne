abb8abf8dab2961c60588858a46d289544d035c5
Siman <wangsiman-carol@outlook.com>
2020-05-20 22:38:17 -0700

    correct linkedlist

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -6,14 +6,14 @@ int main (int argc, const char* argv[]) {
 
     std::atomic<struct list *> ll;
     ll = new struct list();
-    init_multithread_gc(&ll);
+    init_multithread(&ll);
 
-    for (int num_thread = 2; num_thread < 3; num_thread *=2) {
+    for (int num_thread = 2; num_thread < 129; num_thread *=2) {
         auto start = std::chrono::system_clock::now();
 
         std::vector<std::thread> threads(num_thread);
         for (int i = 0; i < num_thread; i++) {
-            threads[i] = std::thread(test_multithread_gc, &ll, i, num_thread);
+            threads[i] = std::thread(test_multithread, &ll, i, num_thread);
         }
         for (int i = 0; i < num_thread; i++) {
             threads[i].join();
@@ -23,8 +23,8 @@ int main (int argc, const char* argv[]) {
         auto elapsed = 
             std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
         
-        std::cout << elapsed.count() << '\n';
+        std::cout << num_thread << " threads took " << elapsed.count() << '\n';
     }
     ll.load()->delete_elems();
-    delete(ll);
+    // delete(ll);
 }
